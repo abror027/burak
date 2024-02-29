886456b6f1d251bcb394abb7241d6bd534c43a44
MAKHKAMOV ABRORJON <157876789+abror027@users.noreply.github.com>
2024-02-29 15:49:58 +0900

    fix: train.js

--- src/train.ts
+++ src/train.ts
@@ -1,16 +1,20 @@
 console.log("hello world")
 
+// H-TASK: 
 
+// shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
+// MASALAN: getPositive([1, -4, 33, 2]) return qiladi "12"
 
-
-let a = [1, -4, 33,2];
-
+let a = [1, -4, 33, 2];
 let b: string = "";
 
-
-for(let i = 0; i < a.length; i++) {
-    if(a[i] > 0) {
-         b += a[i].toString()
+function getPositive(a: Array<number>) {
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > 0) {
+            b += a[i];
         }
     }
-    console.log(b)
+    return b;
+}
+
+console.log(getPositive(a));

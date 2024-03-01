e9677e3eb0c9ea52377f37d4f08b476bf5f47106
MAKHKAMOV ABRORJON <157876789+abror027@users.noreply.github.com>
2024-03-01 23:43:58 +0900

    feat: create restaurant member releted router get and post endpoints

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -7,6 +7,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
     try {
         console.log("goHome")
         res.send("Home Page");
+        // send | json | end | render
     }catch (err) {
         console.log("Error, goHome:", err);
     }restaurantController
@@ -20,12 +21,31 @@ restaurantController.getLogin = (req: Request, res: Response) => {
         console.log("Error, getLogin:", err);
     }
 };
+
 restaurantController.getSignup = (req: Request, res: Response) => {
     try {
         console.log("getSignup")
         res.send("Signup Page");
     }catch (err) {
         console.log("Error, getSignup:", err);
+    }
+};
+
+restaurantController.processLogin = (req: Request, res: Response) => {
+    try {
+        console.log("processLogin")
+        res.send("Meni post bilan oldiz")
+    }catch (err) {
+        console.log("Error, processLogin:", err);
+    }
+};
+
+restaurantController.processSignup = (req: Request, res: Response) => {
+    try {
+        console.log("processSignup")
+        res.send("Meni processSignup danman post bilan oldiz")
+    }catch (err) {
+        console.log("Error, processSignup:", err);
     }
 };
 

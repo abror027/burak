04e0de420221aab877ed368d6539ccefd3eac09c
MAKHKAMOV ABRORJON <157876789+abror027@users.noreply.github.com>
2024-03-07 10:08:40 +0900

    o'zim uchun

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { MemberInput } from "../libs/types/member";
+import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
 const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
     try {
-        console.log("goHome")
+        console.log("goHome");
         res.send("Home Page");
         // send | json | end | render
     }catch (err) {
@@ -17,7 +17,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 
 restaurantController.getLogin = (req: Request, res: Response) => {
     try {
-        console.log("getLogin")
+        console.log("getLogin");
         res.send("Login Page");
     }catch (err) {
         console.log("Error, getLogin:", err);
@@ -26,26 +26,33 @@ restaurantController.getLogin = (req: Request, res: Response) => {
 
 restaurantController.getSignup = (req: Request, res: Response) => {
     try {
-        console.log("getSignup")
+        console.log("getSignup");
         res.send("Signup Page");
     }catch (err) {
         console.log("Error, getSignup:", err);
     }
 };
 
-restaurantController.processLogin = (req: Request, res: Response) => {
+restaurantController.processLogin = async (req: Request, res: Response) => {
     try {
-        console.log("processLogin")
-        res.send("Meni post bilan oldiz")
+        console.log("processLogin");
+        console.log("body", req.body);
+        const input: LoginInput = req.body;
+        
+        const memberService = new MemberService();
+        const result = await memberService.processLogin(input);
+
+        res.send(result)
     }catch (err) {
         console.log("Error, processLogin:", err);
+        res.send(err);
     }
 };
 
 restaurantController.processSignup = async (req: Request, res: Response) => {
     try {
-        console.log("processSignup")
-        console.log("body:", req.body)
+        console.log("processSignup");
+        console.log("body:", req.body);
 
         const newMember: MemberInput = req.body;
         newMember.memberType = MemberType.RESTAURANT;

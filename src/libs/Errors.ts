0783ebb39759028b19cf88bc3ea7b07cf0468d95
MAKHKAMOV ABRORJON <157876789+abror027@users.noreply.github.com>
2024-03-12 11:45:50 +0900

    feat: create checkAuthSession api

--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -18,6 +18,7 @@ export enum Message {
     USED_NICK_PHONE = "Your are inserting alredy used nick or phone!",
     NO_MEMBER_NICK = "No member with that member nick!",
     WRONG_PASSWORD = "Wrong password, please try again!",
+    NOT_AUTHENTCATED = "You are not authentcated, please login first"
 }
 
 class Errors extends Error {

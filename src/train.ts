1675d3b4b1e303968e2883ec3342be50267c42ff
MAKHKAMOV ABRORJON <157876789+abror027@users.noreply.github.com>
2024-03-28 08:36:59 +0900

    fix: MIT R-task DONE!

--- src/train.ts
+++ src/train.ts
@@ -2,20 +2,28 @@
 console.log("hello world");
 
 
-// Q-TASK:
+// R-TASK:
 
-// Shunday function yozing, u 2 ta parametrgga ega bolib birinchisi object, ikkinchisi string. Agar string parametr objectni propertysi bolsa true bolmasa false qaytarsin.
-// MASALAN: hasProperty({name: "BMW", model: "M3"}, "model") return true; hasProperty({name: "BMW", model: "M3"}, "year") return false
+// Shunday function yozing, u string parametrga ega bolsin. String "1+2" holatda pass qilinganda string ichidagi sonlar yigindisini number holatda qaytarsin.
+// MASALAN: calculate("1+3") return 4;
 
 // @MITASK
 
+function calculate(str: string) {
+    let num = str.split("+").map(Number);
+    let count = 0;
+    for (let i = 0; i < num.length; i++) {
+        count += num[i];
+    }
 
-
-function hasProperty(obj: object, str: string) { 
-    return str in obj;
+    return count;
 }
 
-console.log(hasProperty({name: "BMW", model: "M3"}, "model"));
+console.log(calculate("1+3"));
+
+
+
+
 
 
 
